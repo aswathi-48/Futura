1cd36e6c460b275739a62faa8dcddaa95e706927
aswathi-48 <137775218+aswathi-48@users.noreply.github.com>
2023-09-27 15:07:58 +0530

    27-09-23

--- React/futura-app/src/App.js
+++ React/futura-app/src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Sample from "./Components/Sample";
+import Sample2 from "./Components/Sample2";
+import Sample3 from "./Components/Sample3";
+import Sign from "./Components/Sign";
+// import Signinpage from "./Signinpage";
+// import Api from "./Components/Api";
+// import LocalStorage from "./LocalStorage";
+import { Appcontext } from "./Components/Context";
+// import Sampleref from "./Components/Sampleref";
+// import { UseReduceregx } from "./Components/UseReduceregx";
+import UsereduceruseIF from './Components/UsereduceruseIF';
+
 
 function App() {
+  const router=createBrowserRouter([
+    {
+      path:'/',
+      element:<Sample/>
+    },
+    {
+      path:'/sample',
+      element:<Sample2/>
+    },
+    {
+      path:'/sample3',
+      element:<Sample3/>
+    },
+    {
+      path:'/sign',
+      element: <Sign/>
+    },
+    {
+      path:'/usereducerif',
+      element:<UsereduceruseIF/>
+    }
+    
+
+    
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    
+    // <div>
+          <Appcontext.Provider value={'aaa'}>
+      {/* <Sample/>
+      <Sample2/>
+      <Sample3/>
+      <Sign/>
+      <Signinpage/> */}
+      {/* <Sampleref/> */}
+      
+      {/* <Style/> */}
+      {/* <Api/> */}
+    
+      {/* <LocalStorage/> */}
+      {/* <UseReduceregx/>
+       */}
+       <RouterProvider router={router}></RouterProvider>
+      
+      </Appcontext.Provider>
+
+
+    // </div>
+
   );
 }
 
